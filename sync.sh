2ca2834fbd95f134fba76f45802bfc7b3aefa309
Mickaël Perrin <dev@mickaelperrin.fr>
2016-07-18 02:01:00 +0200

    [FEATURE] Disable remote fswatch on sync

--- sync.sh
+++ sync.sh
@@ -2,9 +2,12 @@
 
 volume=$1
 port=$2
-buffer=5
+name=$3
+reverse=$4
 lastrun=/tmp${volume}.lastrun
 reexec=/tmp${volume}.reexec
+
+[ -z $reverse ] && fswatch_destination=fswatch-${name}-reverse || fswatch_destination=fswatch-${name}
 
 if [ ! -d $lastrun ] || [ `stat -c %X $lastrun` -le $(( `date +%s` - buffer )) ]; then
     echo "No update since ${buffer} seconds, start syncing"
@@ -12,10 +15,13 @@ if [ ! -d $lastrun ] || [ `stat -c %X $lastrun` -le $(( `date +%s` - buffer )) ]
     [ ! -d $reexec ] && mkdir -p $reexec
     while [ -d $reexec ]; do
         rmdir $reexec
+        echo "Disabling destination fswatch..."
+        supervisorctl stop $fswatch_destination
         echo "Syncing...."
         unison ${volume}.magic -auto -batch -owner -numericids socket://127.0.0.1:${port}
         sleep $buffer
     done
+    supervisorctl start $fswatch_destination
 else
     echo "Buffering..."
     [ -d $reexec ] && touch $reexec || mkdir -p $reexec
