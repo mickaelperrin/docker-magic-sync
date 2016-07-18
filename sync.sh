48072f5ea636bd38a6e1e84bf1059be7c0e48cb3
Mickaël Perrin <dev@mickaelperrin.fr>
2016-07-18 16:56:27 +0200

    [BUGFIX] Bad volumes names

--- sync.sh
+++ sync.sh
@@ -7,20 +7,28 @@ user=$4
 homedir=$5
 reverse=$6
 buffer=10
+
+fswatch_source=fswatch-${name}${reverse}
+# Host to container
+if [ -z $reverse ]; then
+    volume_reverse=${volume}
+    volume=${volume}.magic
+    fswatch_destination=fswatch-${name}-reverse
+# Container to host
+else
+    fswatch_destination=fswatch-${name}
+    volume_reverse=${volume}.magic
+fi
+
 lastrun=/tmp${volume}.lastrun
 reexec=/tmp${volume}.reexec
-
-[ -z $reverse ] && fswatch_source=fswatch-${name} || fswatch_source=fswatch-${name}-reverse
-[ -z $reverse ] && fswatch_destination=fswatch-${name}-reverse || fswatch_destination=fswatch-${name}
-[ -z $reverse ] && volume_reverse=${volume%.*} || volume_reverse=${volume}.magic
-[ -z $reverse ] && volume=${volume}.magic || volume=${volume}
 
 echo "-----------------------------------------------------------------------------------------------------"
 echo "Starting sync"
 echo
 echo "Watcher: fswatch-${name}${reverse}"
-echo "Volume: ${volume}"
-echo "Volume reverse: ${volume_reverse}"
+echo "Source: ${volume}"
+echo "Destination: ${volume_reverse}"
 
 if [ -d /tmp${volume_reverse}.reexec ]; then
     echo "Skipping reverse sync in progress..."
@@ -38,11 +46,11 @@ else
             reexec_time=`stat -c %X $reexec`
             echo "Recexec time is ${reexec_time}"
             echo "Syncing...."
-            sudo -H -n -u $user -- bash -c "cd $homedir && unison ${volume}.magic -auto -batch -owner -numericids -ignore='Path */.idea/' -ignore='Path */.git/' -ignore='Path */.unison*' socket://127.0.0.1:${port}" >> /tmp/unison.log
-            if [ $(( `date +%s` - $reexec_time )) -le $buffer ]; then
-                echo "Quick sync, sleeping while buffering"
-                sleep $buffer
-            fi
+            sudo -H -n -u $user -- bash -c "cd $homedir && unison ${volume%.*}.magic -auto -batch -owner -numericids -ignore='Path */.idea/' -ignore='Path */.git/' -ignore='Path */.unison*' socket://127.0.0.1:${port}" 2>&1
+#            if [ $(( `date +%s` - $reexec_time )) -le $buffer ]; then
+#                echo "Quick sync, sleeping while buffering"
+#                sleep $buffer
+#            fi
             if [ `stat -c %X $reexec` -eq $reexec_time ]; then
                 echo "No watch triggered during sync. Nothing to sync"
                 rmdir $reexec
